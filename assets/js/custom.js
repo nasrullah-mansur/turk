90960a5d3a3b8d5557027268ab173182c5c29571
nasrullah.mansur <nasrullah.cit.bd@gmail.com>
2021-11-02 12:59:03 +0600

    project start

---
+++ assets/js/custom.js
@@ -0,0 +1,22 @@
+$(document).ready(function() {
+    "use strict";
+
+    $('.banner .banner-slide').slick({
+        slideToScroll: 1,
+        slideToShow: 1,
+        dots: true,
+        prevArrow: '<button type="button" class="slick-prev"><i class="fas fa-arrow-left"></i></button>',
+        nextArrow: '<button type="button" class="slick-next"><i class="fas fa-arrow-right"></i></button>',
+    });
+
+    $('.testimonial .slider').slick({
+        slideToScroll: 1,
+        slideToShow: 1,
+        dots: true,
+        arrows: false,
+    });
+
+    $('.shipping select').niceSelect();
+
+
+});
